911be312ecdc766b706a53dd2e87f639b4272bcc
Thanh-Nghi Do <21120292@student.hcmus.edu.vn>
2023-12-01 23:26:20 +0700

    hehe

--- Lab 2 - Logic/PS4/SCR/main.py
+++ Lab 2 - Logic/PS4/SCR/main.py
@@ -8,19 +8,19 @@ def main():
     folder_path = os.path.dirname(script_path)
 
     #lay tat ca cac input
-    input_files = glob.glob(folder_path + "\\INPUT\\*")
+    input_files = glob.glob(folder_path + "/INPUT/*")
     
     for i in range(len(input_files)):
-        input_files[i] = input_files[i].split("\\INPUT\\")[1]
+        input_files[i] = input_files[i].split("/INPUT/")[1]
 
     for file in input_files:
-        KB,alpha = algorithm.read_input(folder_path + "\\INPUT\\" + file)
+        KB,alpha = algorithm.read_input(folder_path + "/INPUT/" + file)
         input_id = file.split("input")[1]
         output_file = "output" + input_id
         temp,out = algorithm.PL_RESOLUTION(KB,alpha)
-        algorithm.write_output(folder_path + "\\OUTPUT\\" + output_file,out,temp)
+        algorithm.write_output(folder_path + "/OUTPUT/" + output_file,out,temp)
 
-if __file__ == "__main__":
+if __name__ == "__main__":
     main()
 
 
